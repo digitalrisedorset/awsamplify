175522254f2f180fa025fb89c446a8e9d21892e6
digitalrisedorset <herve@digitalrisedorset.co.uk>
2024-04-21 11:11:29 +0100

    Added cognito file

---
+++ js/README.md
@@ -0,0 +1,2 @@
+README.md
+user pool id: 
---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-2_1SoG0Pqjl', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '57gtibih9qu0ikmqb9d6th8hs9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
